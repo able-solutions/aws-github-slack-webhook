013eecb200b93187fc17dc3162de912dadd19bb8
Able710 <able710@protonmail.com>
2019-05-26 15:15:12 +0100

    Added new slack message format for new webhook's created with Github repositories

--- slack.js
+++ slack.js
@@ -50,14 +50,11 @@ var postMessage = async function(slackMessage, callback) {
 
 exports.handler = async (event, context) => {
   
-  const payload = JSON.stringify(event);
+  //const payload = JSON.stringify(event);
   
-  //Get Branch Details
-  const branch = (event.ref).split('/')[2];
-  
-  //Check for Commit Event
   if (event.hasOwnProperty('commits')) {
     eventType = "New Commit Detected";
+    let branch = (event.ref).split('/')[2];
     const commits = event.commits[0];
     const commitMessage = commits.message;
     const committer = commits.committer.name;
@@ -107,6 +104,47 @@ exports.handler = async (event, context) => {
 
     }
     
+  }
+  
+  if (event.hasOwnProperty('hook')) {
+    eventType = "New Web Hook Created";
+    const zen = event.zen;
+    let timestamp = moment(event.hook.created_at).format('llll');
+    const repository = event.repository.name;
+    const repoLink = event.repository.html_url;
+    let color = "#F7EF00";
+    
+    //Hook Slack Message
+    var slackHookMessage = {
+      channel: slackChannel,
+      username: slackUsername,
+      icon_emoji: emoji,
+      attachments: [
+        {
+          "color": color,
+          "author_name": eventType
+        },
+        {
+          "color": color,
+          "fields": [
+            { "title": `Date & Time Hook Created`, "value": `${timestamp}`, "short": false },
+            { "title": `Zen Message of the Day`, "value": `${zen}`, "short": false },
+            { "title": `Repository`, "value": `${repository}`, "short": true },
+            { "title": `Repository Link`, "value": `${repoLink}`, "short": false }
+          ]
+        }
+      ]
+    };
+
+    try {
+
+      await postMessage(slackHookMessage);
+
+    } catch(e) {
+
+      console.error(e);
+
+    }
   }
   
 };
