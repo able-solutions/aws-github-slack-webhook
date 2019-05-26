4a891e21469c0e224787d2a244a4cb1ad82f9f3b
Able710 <able710@protonmail.com>
2019-05-26 13:02:35 +0100

    Added console.log event commands to the slack.js file

---
+++ slack.js
@@ -0,0 +1,9 @@
+const AWS = require('aws-sdk');
+const moment = require('moment');
+
+exports.handler = function(event, context) {
+  
+  console.log(event);
+  console.log(JSON.stringify(event));
+  
+};
